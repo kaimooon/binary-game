7bba039c26c7b3568aa9776a2d9d4e8ba3507d94
kaimooon <118114390+kaimooon@users.noreply.github.com>
2024-03-19 14:12:13 +0800

    MainWindow.xaml.cs

--- binary game/MainWindow.xaml.cs
+++ binary game/MainWindow.xaml.cs
@@ -28,7 +28,6 @@ namespace binary_game
 
         private void start_btn_Click(object sender, RoutedEventArgs e)
         {
-            // Open the Name window
             Name nameWindow = new Name();
             nameWindow.PlayerNameSubmitted += NameWindow_PlayerNameSubmitted;
             nameWindow.ShowDialog();
@@ -36,24 +35,20 @@ namespace binary_game
 
         private void NameWindow_PlayerNameSubmitted(object sender, string playerName)
         {
-            this.playerName = playerName; // Update playerName field
-            // Initialize the game window and pass the player's name
+            this.playerName = playerName;
             Game gameWindow = new Game(playerName);
             gameWindow.ShowDialog();
-            // Initialize the game after the game window is shown
             gameWindow.InitializeGame();
         }
 
         private void ldb_btn_Click(object sender, RoutedEventArgs e)
         {
-            // Open the leaderboard window and pass the player's name
             Leaderboard leaderboardWindow = new Leaderboard(playerName);
             leaderboardWindow.ShowDialog();
         }
 
         private void exit_btn_Click(object sender, RoutedEventArgs e)
         {
-            // Close the application when the Exit button is clicked
             Application.Current.Shutdown();
         }
     }
